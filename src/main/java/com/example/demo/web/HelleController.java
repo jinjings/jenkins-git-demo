c9dcf867c0fe24def29af98e7562114b32777aa0
user <jinjing_0316@126.com>
2021-02-08 14:28:33 +0800

    创建

---
+++ src/main/java/com/example/demo/web/HelleController.java
@@ -0,0 +1,21 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author jj
+ * @version 1.0
+ * @description: TODO
+ * @date 2021/2/8 13:49
+ */
+@RestController
+public class HelleController {
+
+    @RequestMapping("hello")
+    public String sayHello(String name){
+
+        return name.concat(" say hello!!!!!!!");
+    }
+
+}
